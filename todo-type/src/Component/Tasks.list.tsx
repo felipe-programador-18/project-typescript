8910098820d74f7c5ae6e4c86f021fa9919d1b17
Felipe <machadofelipe2016@outlook.com>
2022-09-08 14:10:27 -0300

    c4-get start create tasks list

---
+++ todo-type/src/Component/Tasks.list.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface IAppProps {
+
+}
+
+const TaksList =  (props: IAppProps) =>  {
+  
+    return (
+    <div>
+      <p>List of tasks</p>
+    </div>
+  );
+}
+
+
+export default TaksList
