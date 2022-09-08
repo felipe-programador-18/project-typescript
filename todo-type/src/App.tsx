24685f6649844cb48b3776efbf4d273958b9aa20
Felipe <machadofelipe2016@outlook.com>
2022-09-08 12:40:14 -0300

    c3-rearrange code

--- todo-type/src/App.tsx
+++ todo-type/src/App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import Header from './Component/header/header';
 import Footer from './Component/footer/footer';
 
+import styles from "./app.module.css"
 
 
 function App() {
   return (
     <div>
       <Header/>
+        <main className={styles.main} >
+        <h2>Inside here is my insights!!!</h2>
+        </main>
       <Footer/>
     </div>
   );
