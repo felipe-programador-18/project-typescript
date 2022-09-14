6de3f28ab6ced7b6dbff4a40d6ac0e44af62d0b3
Felipe <machadofelipe2016@outlook.com>
2022-09-14 11:42:37 -0300

    c17-createad a modal her

--- todo-type/src/App.tsx
+++ todo-type/src/App.tsx
@@ -2,10 +2,13 @@
 import{useState} from 'react';
 import Header from './Component/header/header';
 import Footer from './Component/footer/footer';
+import Modal from './Component/Modal';
 
 import styles from "./app.module.css"
 import Taksform from './Component/Tasks.form';
 import TaksList from './Component/Tasks.list';
+
+
 
 import {ITasks} from './Interface/Tasks'
 
@@ -13,23 +16,37 @@ import {ITasks} from './Interface/Tasks'
 function App() {
   const [taksItem,setTaskList] = useState<ITasks[]>([])
   
+
+  const deletedTasks = (id:number) => {
+   setTaskList(
+    taksItem.filter(task => {
+      return task.id !== id
+    })
+   )
+  }
+
   
   return (
     <div>
+     <Modal/> 
       <Header/>
         <main className={styles.main} >
 
          <div>
            <h2>What do you do ?</h2>
             <Taksform 
-            btnSend="Create tasks" 
-            taksItem={taksItem}
-            setTaskList={setTaskList} />
+             btnSend="Create tasks" 
+             taksItem={taksItem}
+             setTaskList={setTaskList} />
          </div>  
         
          <div>
-             <h1>Your daily tasks:</h1>
-             <TaksList  taskList={taksItem}/>
+            <h1>Your daily tasks:</h1>
+            <TaksList 
+              taskList={taksItem} 
+              handleDeleted={deletedTasks}
+              
+              />
              
          </div> 
         
