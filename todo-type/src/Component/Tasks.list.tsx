8a8420b85681be47cc68ac90aea91d6c5003482f
Felipe <machadofelipe2016@outlook.com>
2022-09-14 08:36:57 -0300

    c14-adding a lot of things here

--- todo-type/src/Component/Tasks.list.tsx
+++ todo-type/src/Component/Tasks.list.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import styles from './task.list.module.css'
+import { ITasks } from '../Interface/Tasks';
+
 
 export interface IAppProps {
-
+taskList: ITasks[]
 }
 
-const TaksList =  (props: IAppProps) =>  {
+const TaksList =  ({taskList}: IAppProps) =>  {
   
     return (
-    <div>
-      <p>List of tasks</p>
-    </div>
+    <>
+      {taskList.length === 0 ? ( <div> 
+        <p>
+        Don't have tasks here!
+      </p>
+      </div>
+        ) : 
+       ( <p>
+        {taskList.map((tasks) => (
+        <div className={styles.tasks} 
+         key={tasks.id} >
+          <div className={styles.details} >
+            <h4> {tasks.title} </h4> 
+            <p> {tasks.hard}</p>
+          </div>
+          
+          <div className={styles.action} >
+           <i className='bi bi-pencil' > </i>
+           <i className='bi bi-trash' ></i>
+          </div>
+      
+        </div>)) }
+      </p>) }
+    </>
   );
 }
 
